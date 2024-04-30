179c59d069e27b9e794264e75a4e1adde9fb1046
Lucheng Zhang <geekLucian@gmail.com>
2024-04-29 20:55:31 -0700

    test: Improve test framework

--- tests/usb2.c
+++ tests/usb2.c
@@ -4,13 +4,28 @@
 #define USB2_LOOPBACK_W 0x7000
 #define USB2_LOOPBACK_R 0x7004
 
-int main(void)
-{
-  int res = 0;
-  printf("res before reading=%d\n", res);
-  reg_write32(USB2_LOOPBACK_W, 1);
-  res = reg_read32(USB2_LOOPBACK_R);
-  printf("res after reading=%d\n", res);
+int current_test = 1;
+const int TOTAL_TESTS = 4;
+
+void test_loopback(uint32_t test_value) {
+  printf("[Test %d/%d]: Testing with value: 0x%08X - ", current_test, TOTAL_TESTS, test_value);
+  reg_write32(USB2_LOOPBACK_W, test_value);
+  uint32_t read_value = reg_read32(USB2_LOOPBACK_R);
+  printf("Read value: 0x%08X - ", read_value);
+
+  if (read_value == test_value) {
+      printf("PASSED\n");
+  } else {
+      printf("FAILED (Expected: 0x%08X, Got: 0x%08X)\n", test_value, read_value);
+  }
+  current_test++;
+}
+
+int main(void) {
+  test_loopback(0x1);
+  test_loopback(0x00000000);
+  test_loopback(0xFFFFFFFF);
+  test_loopback(0xDEADBEEF);
 
   return 0;
 }
